a645227e08b612d8291ce43d5cdcf9eb8b2147cb
Mohammad Ali <aliiyusuf85@gmail.com>
2023-10-03 19:55:39 +0530

    newlyUpdated

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import styles from '../style'
 import { arrowUp } from '../assets'
+import { Link } from 'react-router-dom'
+import "../index.css"
 
 const GetStarted = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    
+    <Link to ="/aboutus"><div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
-          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
-            <span className='text-gradient'>Get</span>
-          </p>
-          <img
+        <Link to ="/aboutus"><p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+            <span className='text-gradient' id='spanGetStarted'>Get</span>
+          </p></Link>
+          <Link to="aboutus"><img
             src={arrowUp}
             alt='arrow'
             className='w-[23px] h-[23px] object-contain'
-          />
+          /></Link>
         </div>
-        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-          <span className='text-gradient'>Started</span>
-        </p>
+        <Link to="/aboutus"><p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient ' id='spanGetStarted'>Started</span>
+        </p></Link>
       </div>
-    </div>
+    </div></Link>
   )
 }
 
