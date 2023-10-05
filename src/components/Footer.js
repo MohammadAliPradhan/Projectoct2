25bec12d458766b6eb560643212adbe2e538e0b7
Mohammad Ali <aliiyusuf85@gmail.com>
2023-10-06 02:56:46 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../style'
 import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -14,7 +15,7 @@ const Footer = () => {
             className='w-[266px] h-[172px] object-contain'
           />
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-            We work you slay thats our team commitment.         
+            We work you slay thats our team commitment.
           </p>
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
@@ -25,11 +26,18 @@ const Footer = () => {
               </h4>
               <ul className='list-none mt-4'>
                 {link.links.map((item, index) => (
-                  <li 
-                    key={item.name} 
+                  <li
+                    key={item.name}
                     className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== link.links.length - 1 ? 'mb-4' : 'mb-0'}`}
                   >
-                    {item.name}
+                    {item.name === 'Terms & Services' ? (
+                      <Link to="./terms">{item.name}</Link>
+                    ) : (
+                      item.name
+                    )}
+
+
+
                   </li>
                 ))}
               </ul>
@@ -40,7 +48,7 @@ const Footer = () => {
       <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
           2023 Webservice All Rights Reserved.
-        </p>  
+        </p>
         <div className='flex flex-row md:mt-0 mt-6'>
           {socialMedia.map((social, index) => (
             <img
@@ -50,7 +58,7 @@ const Footer = () => {
               className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
             />
           ))}
-        </div>    
+        </div>
       </div>
     </section>
   )
